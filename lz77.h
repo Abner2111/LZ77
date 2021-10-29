fd623cd6535297d3b4201f77e3d23f452e3b8892
Abner2111 <38987646+Abner2111@users.noreply.github.com>
2021-10-29 15:58:15 -0600

    edited lz77

--- lz77.h
+++ lz77.h
@@ -1,11 +1,25 @@
 #ifndef LZ77_H
 #define LZ77_H
+#include <vector>
+#include <string>
+
+using namespace std;
+
 
 
 class LZ77
 {
 public:
     LZ77();
+    vector <string> getCompressed(string);
+    string getDecompressed(vector<string> triplets);
+private:
+    
+
+
+
 };
+
+
 
 #endif // LZ77_H
